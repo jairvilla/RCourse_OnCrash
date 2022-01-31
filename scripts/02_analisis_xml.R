d0e67967dad54ae0a393d00d55a3fbeed3d558cc
jairvilla <jairasir82@gmail.com>
2022-01-31 15:13:48 -0500

    comienzo curso

---
+++ scripts/02_analisis_xml.R
@@ -0,0 +1,13 @@
+
+
+# Analisis de XLM file 
+# Author: Jair Villanueva
+# Date: Feb-22
+# Course: Data Science for health care 
+#........................................
+
+
+# Set up working directory
+
+getwd()            # donde estoy
+setwd("scripts/")  # donde quiero estar
